f73fe6242cb2d497f563c32e3c33c7e040cfc37a
lucyk22 <lucy.kwan22@sitechhs.com>
2022-01-04 12:27:55 -0500

    added stick.png

--- riddler/src/views/Home.vue
+++ riddler/src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
+    <img src="" alt="">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
+  },
+  data() {
+
   },
 };
 </script>
