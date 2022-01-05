e0fb60d8ee030a8107ceed2450e56d23cce6da63
popplio2 <danikbrisk@yahoo.com>
2022-01-04 23:30:47 -0500

    working with sample movement

--- riddler/src/components/ObjectTest.vue
+++ riddler/src/components/ObjectTest.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="user"></div>
+  <div class="user">(^ . ^)</div>
   <img class="object" src="../assets/mushroom.png">
-  <!-- <button @click="moveAvatar">Move avatar</button> -->
+  <button @click="moveAvatar(5)">Avator to 5</button>
+  <button @click="moveAvatar(10)">Avator to 10</button>
+  <button @click="moveAvatar(20)">Avator to 20</button>
+  <button @click="moveAvatar(30)">Avator to 30</button>
+  <button @click="moveAvatar(40)">Avator to 40</button>
+
   <h1>{{ offset }}</h1>
 </template>
 
@@ -13,30 +18,25 @@ export default {
             offset: null,
         }
     },
-    computed: {
-        returnOffset() {
-            // const userLocation = document.querySelector(".user").getBoundingClientRect();
-            // const objectLocation = document.querySelector(".object").getBoundingClientRect();
-            // const offset = userLocation.left - objectLocation.left;
-            return this.offset;
-        }
-    },
     mounted() {
-        const userLocation = document.querySelector(".user").getBoundingClientRect();
-        const objectLocation = document.querySelector(".object").getBoundingClientRect();
-        const userMidpoint = Math.abs(userLocation.left + userLocation.width) / 2;
-        const objectMidpoint = Math.abs(objectLocation.left + objectLocation.width) / 2;
-        const offset = userMidpoint - objectMidpoint;
-        this.offset = Math.abs(offset);
         this.objectInteract();
     },
     methods: {
-        // moveAvatar() {
-        //     document.querySelector(".user").style.marginLeft = "23rem";
-        // },
+        moveAvatar(margin) {
+            document.querySelector(".user").style.marginLeft = `${margin}rem`;
+            this.objectInteract();
+        },
         objectInteract() {
+            const userLocation = document.querySelector(".user").getBoundingClientRect();
+            const objectLocation = document.querySelector(".object").getBoundingClientRect();
+            const userMidpoint = Math.abs(userLocation.left + userLocation.width) / 2;
+            const objectMidpoint = Math.abs(objectLocation.left + objectLocation.width) / 2;
+            const offset = userMidpoint - objectMidpoint;
+            this.offset = Math.abs(offset);
             if (this.offset <= 30) {
                 document.querySelector(".object").style.border = ".2rem solid yellow";
+            } else {
+                document.querySelector(".object").style.border = ".2rem solid transparent";
             }
         }
     }
@@ -49,9 +49,9 @@ export default {
         bottom: 0;
         width: 5rem;
         height: 7rem;
-        margin-left: 23rem;
+        margin-left: 0;
         z-index: 1;
-        background-color: #0000ff;
+        background-color: rgb(238, 216, 183);
     }
     img {
         position: absolute;
