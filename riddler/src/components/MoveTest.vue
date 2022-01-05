f86718b444a1c5f9be8bdbf6c700c9a4d476538e
lucyk22 <lucy.kwan22@sitechhs.com>
2022-01-05 12:30:08 -0500

    trying to tie the stick to a vue img thing

---
+++ riddler/src/components/MoveTest.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img v-bind:src="test" alt="" class="stick">
+    <img src="../assets/stick.png" alt="" class="stick">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      player: {
+          idle: "../assets/stick.png",
+          left: "../assets/stick_l.png",
+          right: "../assets/stick_r.png",
+      },
+      test: "../assets/stick.png",
+    }
+  }
+
+}
+</script>
+
+<style>
+  .stick {
+    width: 15%;
+
+  }
+</style>
