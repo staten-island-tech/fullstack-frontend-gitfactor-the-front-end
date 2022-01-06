2be651d06b937204901ee2c8ae3e82cb45c4c6e3
lucyk22 <lucy.kwan22@sitechhs.com>
2022-01-06 12:26:19 -0500

    keypress functions

--- riddler/src/components/MoveTest.vue
+++ riddler/src/components/MoveTest.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <img v-bind:src="test" alt="" class="stick">
-    <img src="../assets/stick.png" alt="" class="stick">
+    <div class="avatar">
+      <img v-bind:src="test" alt="" class="stickAvatar">
+      <img src="../assets/stick.png" alt="" class="stick" id="stickAvatar">
+      <input class="tempInput" v-on:keyup.left="leftMove();" v-on:keyup.right="rightMove();">
+    </div>
   </div>
 </template>
 
@@ -9,14 +12,24 @@
 export default {
   data() {
     return {
-      player: {
-          idle: "../assets/stick.png",
-          left: "../assets/stick_l.png",
-          right: "../assets/stick_r.png",
-      },
       test: "../assets/stick.png",
+      pos: `80%`,
     }
-  }
+  },
+  methods: {
+    leftMove: function() {
+      console.log("left");
+      const avatar = document.getElementById("stickAvatar");
+      this.pos = "20%"
+      avatar.style.left = this.pos;
+    },
+    rightMove: function() {
+      console.log("right")
+      const avatar = document.getElementById("stickAvatar");
+      this.pos = "80%"
+      avatar.style.left = this.pos;
+    }
+  },
 
 }
 </script>
@@ -24,6 +37,17 @@ export default {
 <style>
   .stick {
     width: 15%;
+    display: flex;
+    position:relative;
+    left: 80%;
 
+  }
+
+  .hide {
+    display: none;
+  }
+
+  .tempInput {
+    font-size: 10rem;
   }
 </style>
