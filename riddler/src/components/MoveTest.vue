dffefa7763d8db27a221ced1635731cb740acb25
lucyk22 <lucy.kwan22@sitechhs.com>
2022-01-12 11:51:59 -0500

    trying to undo whatever i did that broke my code

--- riddler/src/components/MoveTest.vue
+++ riddler/src/components/MoveTest.vue
@@ -1,63 +1,26 @@
 <template>
-  <easel-canvas>
-    <div class="avatar">
-      <img src="../assets/stick.png" alt="" class="stick" id="stickAvatar">
-      <easel-bitmap image="/assets/stick.png"
-      :x="0"
-      :y="0"
-      :align="['left','top']">
-      <input class="tempInput" v-on:keyup.left="leftMove();" v-on:keyup.right="rightMove();">
-    </div>
-
-  </easel-canvas>
+  <div>
+    <img v-bind:src="test" alt="" class="stick" />
+    <img src="../assets/stick.png" alt="" class="stick" />
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      player: {
+          idle: "../assets/stick.png",
+          left: "../assets/stick_l.png",
+          right: "../assets/stick_r.png",
+      },
       test: "../assets/stick.png",
-      pos: `80%`,
     }
-  },
-  created() {
-    this.createRPG()
-  },
-  methods: {
-    createRPG: function() {
+  }
 
-    },
-    leftMove: function() {
-      console.log("left");
-      const avatar = document.getElementById("stickAvatar");
-      this.pos = "20%"
-      avatar.style.left = this.pos;
-    },
-    rightMove: function() {
-      console.log("right")
-      const avatar = document.getElementById("stickAvatar");
-      this.pos = "80%"
-      avatar.style.left = this.pos;
-    }
-  },
 
 }
 </script>
 
 <style>
-  .stick {
-    width: 15%;
-    display: flex;
-    position:relative;
-    left: 80%;
-
-  }
-
-  .hide {
-    display: none;
-  }
-
-  .tempInput {
-    font-size: 10rem;
-  }
 </style>
