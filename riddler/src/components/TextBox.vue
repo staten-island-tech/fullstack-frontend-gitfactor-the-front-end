46c4b0dfa92069127ae5bf046b6f4c0c3959515c
Charley13 <charley.baluja22@sitechhs.com>
2022-01-29 18:35:36 -0500

    working on text update functionality

--- riddler/src/components/TextBox.vue
+++ riddler/src/components/TextBox.vue
@@ -1,9 +1,9 @@
 <template>
   <div >
-    <button v-on:click="txtbxShow" class="textbox-button">Enter</button>
+    <button v-on:click="txtbxShow" class="textbox-button">Z</button>
     <div v-if="txtbx" class="textbox">
       <p class="textbox-title">Count Dracula</p>
-<p class="textbox-test">{{textArray[0].text}}</p>
+<p class="textbox-test">{{ textArray[0].text }}</p>
     </div>
   </div>
 </template>
@@ -17,41 +17,54 @@ textArray: [
   {
     text: "Mwahaha welcome to my castle~",
     characterID: 1, 
+    order: 1,
   }, 
   {
      text: "I didn't come here by choice...what is this?",
     characterID: 0, 
+    order: 2,
   },
   {
      text: "This...huh. I don't actually know what this is either! I just said that cause I thought it was spooky :D",
     characterID: 1, 
+    order: 3, 
   }
 ], 
  txtbx: false,
-txtbxShow(){
-this.txtbx = true;
-const dialogue = document.querySelector(".textbox-test").innerHTML;
- console.log(dialogue)
- var btn = document.querySelector(".textbox-button");
- btn.addEventListener("click", function() {
-  let counter = 0;
-counter += 1;
-  console.log(counter);
-});
-},
+ count: 0,
   }
+}, 
+methods: {
+  txtbxShow(){
+this.txtbx = true;
+this.count +=1;
+// console.log(this.count);
+const textOutput = document.querySelector(".textbox-test");
+console.log(textOutput.innerHTML);
+// if(this.count === this.textArray.order){
+//   textOutput.innerHTML = this.textArray[2].text;
+// }
+this.textArray.forEach(object => {
+  if(this.count === object.order){
+    console.log("hi")
+  }
+});
+}
 }
 }
 </script>
 
 <style>
 .textbox-button{
-  font-size: 3rem;
-  background-color: black;
-  color: red;
+  font-size: 4rem;
+  background-color: #766696;
+  color: #DECEFF;
   position: absolute;
-  top: 40%;
+  top: 30%;
   left: 40%;
+  border-radius: 50%;
+  border:#DECEFF solid 0.3rem ;
+  padding: 2rem;
 }
 .textbox{
   border: red 1rem solid;
