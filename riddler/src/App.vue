8a091a4b37d1b53f8d788f34f6d2ad390a84c15f
Charley13 <charley.baluja22@sitechhs.com>
2022-02-14 12:26:31 -0500

    copy pasted Alston article

--- riddler/src/App.vue
+++ riddler/src/App.vue
@@ -1,47 +1,58 @@
+<script setup>
+import { useAuth0, AuthState } from "./utils/useAuth0";
+const { login, logout, initAuth } = useAuth0(AuthState);
+
+initAuth();
+</script>
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <div v-if="!$auth.loading.value">
-      <button v-if="!$auth.isAuthenticated.value" @click="login">Log in</button>
-      <button v-if="$auth.isAuthenticated.value" @click="logout">Log out</button>
+  <div v-if="!AuthState.loading">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div v-if="!AuthState.isAuthenticated">
+      <button @click="login()" class="btn btn-primary">Login</button>
+    </div>
+
+    <div v-else>
+      <p>
+        Welcome to VueAuth <strong>{{ AuthState.user.name }}</strong>
+      </p>
+      <button @click="logout()" class="btn btn-secondary">Logout</button>
     </div>
   </div>
-  <router-view/>
+
+  <div v-else>Loading ...</div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  methods: {
-    login() {
-      this.$auth.loginWithRedirect();
-    },
-    // Log the user out
-    logout() {
-      this.$auth.logout({
-        returnTo: window.location.origin
-      });
-    }
-  }
-}
-</script>
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.btn {
+  padding: 8px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.5;
+  border: none;
+  cursor: pointer;
+  min-width: 100px;
+  border-radius: 4px;
+  font-weight: bold;
+}
+
+.btn-primary {
+  background: #41b883;
+  color: white;
+}
+
+.btn-secondary {
+  background: #aaa;
+  color: white;
 }
 </style>
