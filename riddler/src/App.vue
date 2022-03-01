23f31a6bde65eab9a547189c0047bf9b4a733fb9
popplio2 <danikbrisk@yahoo.com>
2022-03-01 12:34:10 -0500

    created game item object

--- riddler/src/App.vue
+++ riddler/src/App.vue
@@ -6,6 +6,89 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      gameItems: {
+        levelOne: {
+          sectionOne: [
+            {
+              name: "Riddler",
+              id: 1,
+              position: 10,
+              margin: "10%",
+              img: "riddl.png",
+              isInteractable: false,
+              filter: null,
+              itemType: "character", //could also be puzzle, object, etc.
+              dialogueSprite: "sprite_dialogue_riddl.png",
+              dialogue: [
+                {
+                  text: "Mwahaha welcome to my tower~",
+                  name: "???",
+                  isAntagonist: true,
+                }, 
+                {
+                  text: "I didn't come here by choice...what is this?",
+                  name: "Me"
+                },
+                {
+                  text: "This...huh. I don't actually know what this is either! I just said that cause I thought it was spooky :D",
+                  name: "???",
+                  isAntagonist: true,
+                }
+              ],
+            }, 
+          ],
+          sectionTwo: [
+            {
+              name: "Mushroom",
+              id: 0,
+              position: 40,
+              margin: "40%",
+              img: "mushroom.png",
+              isInteractable: false,
+              filter: null,
+              itemType: "object", 
+              prompt: "Do you want to pick up this amazing mushroom house?",
+            },
+          ],
+          sectionThree: [
+            {
+              name: "Protagonist",
+              id: 2,
+              position: 70,
+              margin: "70%",
+              img: "prt.png",
+              isInteractable: false,
+              filter: null,
+              itemType: "character",
+              dialogueSprite: "sprite_dialogue_player.png", 
+              dialogue: [
+                {
+                  text: "What? Am I talking to myself",
+                  name: "Me?",
+                  isAntagonist: true,
+                }, 
+                {
+                  text: "Wat",
+                  name: "Me"
+                },
+                {
+                  text: "This...huh. I don't actually know what this is either! I just said that cause I thought it was spooky :D",
+                  name: "Me?",
+                  isAntagonist: true,
+                }
+              ],
+            }
+          ],
+        },
+      }
+    }
+  }
+}
+</script>
 <style>
 *, html, body {
   font-size: 62.5%;
