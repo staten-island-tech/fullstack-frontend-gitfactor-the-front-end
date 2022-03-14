75746b8516573e084400a764097cbb9ab0c8ed8f
Charley13 <charley.baluja22@sitechhs.com>
2022-03-14 12:26:56 -0400

    working

--- riddler/src/components/PauseMenu.vue
+++ riddler/src/components/PauseMenu.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="pause-menu">
-      <font-awesome-icon icon="fa-solid fa-gear" />
+      <!-- <font-awesome-icon icon="fa-solid fa-gear" /> -->
+      <h2 class="pause-title">Pause Menu</h2>
+      <button>Instruction Menu</button>
+       <button>Save</button>
+        <button>Save & Exit</button>
+          <button>Settings</button>
   </div>
 </template>
 
@@ -12,8 +17,20 @@ name: "PauseMenu"
 
 <style>
 .pause-menu{
-    width: 100rem;
-    height: 100rem;
-    background-color: black;
+    width: 100%;
+    height: 100%;
+    background-color: #deceff;
+    display: flex;
+    justify-content: center;
+align-content: center;
+}
+.pause-title{
+  color: white;
+  font-size: 3rem;
+}
+.pause-menu button{
+
+background-color: #A68FD5;
+color: black;
 }
 </style>
