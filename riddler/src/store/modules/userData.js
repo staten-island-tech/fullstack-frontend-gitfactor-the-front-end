c2e955063cedd03bbb214cfc95fbdcb0a99ae222
popplio2 <danikbrisk@yahoo.com>
2022-03-16 19:18:26 -0400

    patch and post request working

--- riddler/src/store/modules/userData.js
+++ riddler/src/store/modules/userData.js
@@ -1,4 +1,6 @@
 export const state = {  
+  name: null,
+  id: null,
   level: null,
   section: null,
   leftValue: null,
@@ -15,11 +17,14 @@ export const mutations = {
     state.lifeCount--;
   },
   updateState(state, newState) {
+    state.name = newState[0].name;
+    state.id = newState[0]._id;
     state.level = newState[0].level;
     state.section = newState[0].section;
     state.leftValue = newState[0].leftValue;
     state.lifeCount = newState[0].lifeCount;
     state.currentItem = newState[0].currentItem;
     state.inventory = newState[0].inventory;
+    console.log(state);
   },
 }
