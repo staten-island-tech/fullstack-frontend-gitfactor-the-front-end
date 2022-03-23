53c1e4f53576d1fa6da72029594733571a625652
nika2004 <nika.imamberdieva22@sitechhs.com>
2022-03-23 12:37:54 -0400

    3/23/22

---
+++ riddler/src/components/LoginComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="nav">
+   
+    <div v-if="!$auth.loading.value">
+      <button v-if="!$auth.isAuthenticated.value" @click="login">Log in</button>
+      <button v-if="$auth.isAuthenticated.value" @click="logout">Log out</button>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- riddler/src/main.js
+++ riddler/src/main.js
@@ -4,4 +4,23 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// createApp(App).use(store).use(router).mount("#app");
+
+import authConfig from '../auth_config.json'
+import { setupAuth } from './auth'
+
+let app = createApp(App).use(store).use(router)
+
+function callbackRedirect(appState) {
+  router.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : '/'
+  );
+}
+
+setupAuth(authConfig, callbackRedirect).then((auth) => {
+  app.use(auth).mount('#app')
+})
+
+
