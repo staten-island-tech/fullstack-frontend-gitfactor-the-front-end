9ab09b3a2b2a0c0d3389c2a032dfe42fce52092c
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2022-03-30 11:51:47 -0400

    a

--- riddler/src/components/Inventory.vue
+++ riddler/src/components/Inventory.vue
@@ -20,7 +20,7 @@ export default {
     methods: {
         itemDetails(item) {
             alert(`This item is a ${item.name}.`); 
-            this.$emit('clickedItem');
+            this.$emit('clickedItem', item);
         },
         toggleInventory() {
           this.isInventoryOpen = !this.isInventoryOpen;
