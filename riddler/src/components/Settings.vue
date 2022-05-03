e99897214e9748193a8df0df541c9f52f9e27312
Charley13 <charley.baluja22@sitechhs.com>
2022-05-03 12:25:49 -0400

    added music volume slider (not functional yet)

--- riddler/src/components/Settings.vue
+++ riddler/src/components/Settings.vue
@@ -5,8 +5,16 @@
 </div>
       <h2 class="pause-title settings-title">Settings</h2>
       <div class="pause-button-box settings-box">
-      <button>Music Volume</button>
-       <button>Sound Effect Volume</button>
+
+      <!-- <button>Music Volume</button> -->
+       <!-- <button>Sound Effect Volume</button> -->
+
+    <!-- sliders go here! -->
+        <h2 class="slider-label">Music Volume</h2>
+<div class="slidecontainer">
+     <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+<h3 class="volume"></h3>
         <button>Brightness</button>
            <button>Restart Game</button>
           <button>Delete Account</button>
@@ -16,7 +24,10 @@
 
 <script>
 export default {
-name: "Settings"
+name: "Settings", 
+methods: {
+
+},
 }
 </script>
 
@@ -27,4 +38,49 @@ name: "Settings"
 .settings-box button{
     font-size: 2rem;
 }
+.slider-label{
+    font-size: 2rem;
+    color: black;
+}
+.slidecontainer {
+  width: 20%; /* Width of the outside container */
+}
+
+/* The slider itself */
+.slider {
+  -webkit-appearance: none;  /* Override default CSS styles */
+  appearance: none;
+  width: 100%; /* Full-width */
+  height: 2rem; /* Specified height */
+  border-radius: 1rem;
+  /* background: #d3d3d3; Grey background */
+  outline: none; /* Remove outline */
+  opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+  -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+  transition: opacity .2s;
+}
+
+/* Mouse-over effects */
+.slider:hover {
+  opacity: 1; /* Fully shown on mouse-over */
+}
+
+/* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none; /* Override default look */
+  appearance: none;
+  width: 2.5rem; /* Set a specific slider handle width */
+  height: 2rem; /* Slider handle height */
+  border-radius: 1rem;
+  background: #04AA6D; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+}
+
+.slider::-moz-range-thumb {
+  width: 2rem; /* Set a specific slider handle width */
+  height: 2rem; /* Slider handle height */
+  background: #04AA6D; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+}
+
 </style>
