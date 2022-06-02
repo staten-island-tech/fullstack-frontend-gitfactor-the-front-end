918e4293c1c9526abcb853d1f51495dd417731dd
Daniel Briskman <71455312+popplio2@users.noreply.github.com>
2022-06-02 01:30:59 -0400

    Revert "items, bgm, new sfx added"

--- riddler/src/App.vue
+++ riddler/src/App.vue
@@ -1,57 +1,30 @@
 <template>
- <Login></Login>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
 </template>
-
-<script>
-import Login from "../src/components/LoginComponent.vue"
-
-export default {
-  components: {
-    Login,
-  }
-}
-</script>
 
 <style>
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: var(--background-color);
-  color: var(--highlight-color);
+  color: #2c3e50;
 }
-.login-page {
-  height: 100vh;
-  background-color: #1f1227;
+
+#nav {
+  padding: 30px;
 }
-.welcome, .welcome strong {
-  display: inline;
-  font-size: 2rem;
-}
-.welcome strong {
-  font-weight: 700;
-}
-.btn {
-  padding: .8rem 1.2rem;
-  margin-top: 2rem;
-  margin-left: 2rem;
-  font-size: 1.4rem;
-  font-weight: 400;
-  line-height: 1.5;
-  border: none;
-  cursor: pointer;
-  min-width: 10rem;
-  border-radius: .4rem;
+
+#nav a {
   font-weight: bold;
+  color: #2c3e50;
 }
 
-.btn-primary {
-  background: #b84169;
-  color: white;
-}
-
-.btn-secondary {
-  background: #aaa;
-  color: white;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- riddler/src/main.js
+++ riddler/src/main.js
@@ -4,32 +4,4 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-// createApp(App).use(store).use(router).mount("#app");
-
-import authConfig from '../src/auth_config.json'
-import { setupAuth } from '../src/auth'
-
-let app = createApp(App).use(store).use(router)
-
-function callbackRedirect(appState) {
-  router.push(
-    appState && appState.targetUrl
-      ? appState.targetUrl
-      : '/'
-  );
-}
-
-setupAuth(authConfig, callbackRedirect).then((auth) => {
-  app.use(auth).mount('#app')
-})
-
-
-app.component("font-awesome-icon", FontAwesomeIcon);
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-/* import specific icons */
-import { faPause, faX, faHeart, faPowerOff, faCaretLeft, faCaretRight, faZ } from "@fortawesome/free-solid-svg-icons";
-/* import font awesome icon component */
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-/* add icons to the library */
-library.add(faPause, faX, faHeart, faPowerOff, faCaretLeft, faCaretRight, faZ);
+createApp(App).use(store).use(router).mount("#app");
