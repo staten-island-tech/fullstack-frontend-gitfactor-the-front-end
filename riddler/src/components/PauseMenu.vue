b4a30a1c2f775996498be515e8fc1b3d9927b54d
lucyk22 <71743901+lucyk22@users.noreply.github.com>
2022-06-02 00:50:39 -0400

    pause menu scrollbar delete

--- riddler/src/components/PauseMenu.vue
+++ riddler/src/components/PauseMenu.vue
@@ -14,30 +14,32 @@
 </template>
 
 <script>
-
 export default {
-  name: "PauseMenu", 
+  name: "PauseMenu",
   props: ["isPauseOpen"],
   data() {
     return {
       userdata: this.$auth.user,
-    }
+    };
   },
   methods: {
     async updateData() {
       const userId = this.userdata.sub.replace("auth0|", "");
-      console.log(this.$store.state.userData)
+      console.log(this.$store.state.userData);
       try {
-        const response = await fetch(`http://localhost:3000/api/index/update/${userId}`, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(this.$store.state.userData),
-        });
+        const response = await fetch(
+          `http://localhost:3000/api/index/update/${userId}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.$store.state.userData),
+          }
+        );
         const data = await response.json();
         console.log(data);
-        this.$store.commit('updateState', data);
+        this.$store.commit("updateState", data);
         window.location.reload();
         alert("Your progress has been saved.");
       } catch (error) {
@@ -47,57 +49,63 @@ export default {
     logout() {
       this.updateData();
       this.$auth.logout({
-        returnTo: window.location.origin
+        returnTo: window.location.origin,
       });
     },
-  }
-}
+  },
+};
 </script>
 
 <style>
 @import "../assets/global.css";
 
-  .pause-menu {
-    overflow: scroll;
-    position: absolute;
-    top: 0;
-    text-align: center;
-    width: 100%;
-    height: 100%;
-    background-color: var(--purple);
-    z-index: 3;
-  }
-  .pause-title {
-    color: var(--highlight-color);
-  }
-  .icon-div {
-    display: flex;
-    justify-content: right;
-    padding-right: 6rem;
-    padding-top: 3rem;
-  }
-  .x-icon {
-    position: fixed;
-    top: 3rem;
-    right: 3rem;
-    font-size: var(--h4);
-    color: var(--highlight-color);
-  }
-  .pause-button-box {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 1rem;
-  }
-  .pause-menu button {
-    width: 30%;
-    font-size: 2.5rem;
-    margin: 1rem;
-    padding: 1rem;
-    background-color: var(--highlight-color);
-    color: black;
-    border: black 0.05rem solid;
-    border-radius: 0.5rem;
-  }
+.pause-menu {
+  overflow: scroll;
+  position: absolute;
+  top: 0;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  background-color: var(--purple);
+  z-index: 3;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+.pause-menu::-webkit-scrollbar {
+  display: none;
+}
+.pause-title {
+  color: var(--highlight-color);
+}
+.icon-div {
+  display: flex;
+  justify-content: right;
+  padding-right: 6rem;
+  padding-top: 3rem;
+}
+.x-icon {
+  position: fixed;
+  top: 3rem;
+  right: 3rem;
+  font-size: var(--h4);
+  color: var(--highlight-color);
+}
+.pause-button-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+}
+.pause-menu button {
+  width: 30%;
+  font-size: 2.5rem;
+  margin: 1rem;
+  padding: 1rem;
+  background-color: var(--highlight-color);
+  color: black;
+  border: black 0.05rem solid;
+  border-radius: 0.5rem;
+}
 </style>
